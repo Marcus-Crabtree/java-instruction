8546aa1f05309887bb92ccbf8d231d442515f53f
Marcus Crabtree <kmarcuscrabtree@gmail.com>
2020-03-26 14:17:34 -0400

    debug changes

--- CH05_StaticMethods/src/MethodMadnessCatchStatement.java
+++ CH05_StaticMethods/src/MethodMadnessCatchStatement.java
@@ -1,7 +1,7 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class MethodMadness {
+public class MethodMadnessCatchStatement {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
---
+++ CH05_StaticMethods/src/MethodMadnessDataValidation.java
@@ -0,0 +1,77 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MethodMadnessDataValidation {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		Scanner sc = new Scanner(System.in);
+		String name = "Marcus";
+		printWelcomeMessage();
+		printWelcomeMessage(name);
+
+		// get 2 intergers on one line
+		// System.out.println("Enter 2 intergets (ex. 1 5): ");
+		// int a =sc.nextInt();
+		// int b =sc.nextInt();
+
+		// get int a
+		int a = getValidInt("Enter number 1: ", sc);
+		// get int b
+		int b = getValidInt("Enter number 2: ", sc);
+		
+				
+		System.out.println("Sum is " + sum(a, b));
+		System.out.println("Difference is " + diff(a, b));
+		System.out.println("Quotient is " + quotient(a, b));
+		System.out.println("Product is " + product(a, b));
+
+		System.out.println("goodbye");
+	}
+
+	private static int getValidInt(String prompt, Scanner sc) {
+		int i = 0;
+		while (true) {
+            
+		// print message to user
+		System.out.print(prompt);
+		if (sc.hasNextInt()) {
+		i = sc.nextInt();
+		break;
+		}
+		// reprompt if bad data
+		else {
+			System.out.println("Error, Entry must be ah whole number");
+			sc.nextInt();
+				 		
+		}
+		// return valid input
+		}
+		return i;
+	}
+
+	private static void printWelcomeMessage() {
+		System.out.println("Welcome to the madness");
+	}
+
+	private static void printWelcomeMessage(String name) {
+		System.out.println("Welcome, " + name + ",to the madness!");
+	}
+
+	private static int sum(int n1, int n2) {
+		return n1 + n2;
+	}
+
+	private static int diff(int a, int b) {
+		return a - b;
+	}
+
+	private static int quotient(int a, int b) {
+		return a / b;
+	}
+
+	private static int product(int a, int b) {
+		return a * b;
+	}
+}
+
