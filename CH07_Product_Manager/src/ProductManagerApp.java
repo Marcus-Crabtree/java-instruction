f2714279320db2a92587f95b714d3d0728ecbad3
Marcus Crabtree <kmarcuscrabtree@gmail.com>
2020-03-30 12:29:44 -0400

    ch07 init commit

---
+++ CH07_Product_Manager/src/ProductManagerApp.java
@@ -0,0 +1,23 @@
+
+public class ProductManagerApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to product mgr.");
+		// create some instances of products and display their info
+		//create 2 instances of Product
+		Product p1 = new Product();
+		p1.setCode("Java");
+		p1.setDescription("Murach's Java Programming");
+		p1.setPrice(57.50);
+		System.out.println(p1.toString());
+		System.out.println("Price of p1 is: "+p1.getPriceFormatted());
+		
+		Product p2 = new Product("MySQL", "Murrach's MySQL", 54.50);
+		System.out.println(p2);
+		
+		System.out.println(p1);
+		
+		System.out.println("goodbye.");
+	}
+
+}
