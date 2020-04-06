e27a0a40d7df9be8a9d2fb114ad30cf5a69d67a9
Marcus Crabtree <kmarcuscrabtree@gmail.com>
2020-04-06 10:24:39 -0400

    update

--- CH12_prj2_The Wizzards_Bag/src/WizzardsBagApp.java
+++ CH12_prj2_The Wizzards_Bag/src/WizzardsBagApp.java
@@ -10,7 +10,7 @@ public class WizzardsBagApp {
 		String editItemName = "";
 		int dropItemIdx = 0;
 
-		System.out.println("Welcome to the Wizzards bag!");
+		System.out.println("Welcome to the Wizzards bag! Before you go on your journey, check your \nback and see if you can grab anything else before you head off!");
 
 		ArrayList<String> inventory = new ArrayList<>(3);
 		inventory.add("Wooden Staff");
@@ -21,16 +21,16 @@ public class WizzardsBagApp {
 		choice = Console.getString("\nCommand: ", true);
 
 		// -- begin loop"
-		while (!choice.equalsIgnoreCase("exit")) {
-			if (choice.equalsIgnoreCase("show")) {
+		while (!choice.equalsIgnoreCase("Exit")) {
+			if (choice.equalsIgnoreCase("Show")) {
 				for (int i = 0; i < inventory.size(); i++) {
 					System.out.println(i + 1 + ". " + inventory.get(i));
 				}
 			} 
-			else if (choice.equalsIgnoreCase("grab")) {
+			else if (choice.equalsIgnoreCase("Grab")) {
 
 				if (inventory.size() < 4) {
-					grabItem = Console.getString("Item name: ");
+					grabItem = Console.getString("Type an item you want to add into existence: ");
 					inventory.add(grabItem);
 					System.out.println(grabItem + " was added!");
 				}
@@ -38,14 +38,14 @@ public class WizzardsBagApp {
 					System.out.println("Your bag is full! Drop an item to pick another item up.");
 				}
 			} 
-			else if (choice.equalsIgnoreCase("edit")) {
+			else if (choice.equalsIgnoreCase("Edit")) {
 				editItemIdx = Console.getInt("Number: ", 0, inventory.size() + 1);
 				editItemName = Console.getString("Updated Item: ");
 				inventory.set(editItemIdx - 1, editItemName);
 				System.out.println("Item number: " + (editItemIdx) + " was updated!");
 			}
 
-			else if (choice.equalsIgnoreCase("drop")) {
+			else if (choice.equalsIgnoreCase("Drop")) {
 				dropItemIdx = Console.getInt("Number: ", 0, inventory.size() + 1);
 				System.out.println(
 						inventory.get((dropItemIdx) - 1) + " was dropped!" + inventory.remove(dropItemIdx - 1));
@@ -59,12 +59,12 @@ public class WizzardsBagApp {
 			}
 			choice = Console.getString("\nCommand: ");
 		}
-			System.out.println("later Skater!");
+			System.out.println("The Wizzard smiled as he looked down the path, \npatting his bag confiedntly as he embarked on a new adventure.");
 		}
 
 	public static void commandMenu() {
-		System.out.println("\n List of Commands: \n" + "\nshow - Shows items in bag" + "\nGrab - grab an item"
-				+ "\nEdit - change the name of an item" + "\nHelp - show command list" + "\nexit - exit the program" );
+		System.out.println("\n List of Commands: \n" + "\nShow - Shows items in bag" + "\nGrab - grab an item"
+				+ "\nEdit - change the name of an item" + "\nHelp - show command list" + "\nExit - exit the program" );
 
 	}
 
